03bced20c2ff15027d98f62d969ea9ef69c6428d
AmanPatelcubastion <aman.patel@cubastion.com>
2017-11-17 15:52:29 +0530

    17/11/17_16:00_final

--- main.go
+++ main.go
@@ -18,8 +18,6 @@ import (
 	"io/ioutil"
 	"golang.org/x/crypto/bcrypt"
 
-	"strconv"
-
 )
 
 type Organization struct {
@@ -57,7 +55,6 @@ type Product struct {
 	ID                 uint           `json:"id"`                    // id
 	Name               string         `json:"name"`                  // name
 	DescText           string         `json:"desc_text"`             // desc_text
-//	OrgID              uint           `json:"org_id"`                // org_id
 	Type               string         `json:"pri_demo_id"`           // type
 
 }
@@ -73,7 +70,12 @@ type UserClaims struct {
 }
 
 type Token struct {
-	Token 	string                      `json:"token"`
+	Token  string                      `json:"token"`
+}
+
+type Datastruct struct {                                                 //for JSON Formatting
+	StatusCode string            `json:"statuscode"`
+	Response   interface{}       `json:"response"`
 }
 
 var claims UserClaims
@@ -88,7 +90,6 @@ var (
 	SignKey   *rsa.PrivateKey
 )
 
-var db *gorm.DB
 var err error
 
 func initKeys(){
@@ -108,11 +109,9 @@ func initKeys(){
 	}
 }
 
-func main() {
-
-	//todo connect db with gorm
-	db_name:="xshowroom_auth"                                                    //cubastion , ford india
-	db,err=gorm.Open("mysql","root:password@/"+db_name+"?charset=utf8&parseTime=True&loc=Local")
+func initDb(dbName string) *gorm.DB{
+	var db *gorm.DB
+	db,err=gorm.Open("mysql","root:password@/"+dbName+"?charset=utf8&parseTime=True&loc=Local")
 
 	if err!=nil {
 		log.Fatal(err)
@@ -121,248 +120,205 @@ func main() {
 	err = db.DB().Ping()
 	if err != nil {
 		log.Fatal(err)
+		return nil
 	}else{
 		fmt.Println("connected")
+		return db
 	}
+
+}
+
+func main() {
+	db:=initDb("xshowroom_auth")
 	defer db.Close()
 
-    initKeys()
+	initKeys()
 
-	//todo create routes with gin
 	r:=gin.Default()
-	r.POST("/login",login)
-//	r.POST("/auth_middleware",authMiddleware)
-	r.GET("/products",getProducts)
-	r.GET("/users",getUsers)
-	r.POST("/users",createUsers)
-	r.GET("/organisation",getOrg)
-	r.POST("/organisation",createOrg)
+	r.POST("/login", login)
+
+	group:=r.Group("/api/v1/")
+
+	group.Use(authMiddleWare())
+	{
+		group.GET("products", getProducts)
+		group.GET("users", getUsers)
+		group.POST("users", createUsers)
+		group.GET("organisation", getOrg)
+		group.POST("organisation", createOrg)
+	}
 	r.Run(":8080")
 }
 
 func login(c *gin.Context){
 
-	//credentials by headers
+	//getting credentials from headers
 	name1:=c.Request.Header.Get("username")
 	pwd1:=c.Request.Header.Get("password")
 
 	fmt.Println("FORM RETURNS ::",name1,pwd1)
 
-	var flag int                                                        // =1 , user login success
+	db:=initDb("xshowroom_auth")
+	defer db.Close()
+
 	//validate user credentials
-
 	var user []User
-    db.Model(User{}).Find(&user)
+	db.Model(User{}).Find(&user)
 
+	flag:=-1                                                            //exit condition
 	for _,v:=range user{
-		if strings.ToLower(name1) == v.Email{
-			err:=bcrypt.CompareHashAndPassword([]byte(v.Password),[]byte(pwd1))
-			if err!=nil {
-				fmt.Println("Error logging in")
-				fmt.Fprint(c.Writer, "Invalid credentials")
-				flag =1
-			}else {
-				//set claims
-				claims = UserClaims{
-					v,
-					time.Now(),                       //to generate unique token everytime  //rand.Intn(10000),
-					jwt.StandardClaims{
-						Issuer: "testing_administrator",
-					},
-				}
+
+		if strings.ToLower(name1) == v.Email && bcrypt.CompareHashAndPassword([]byte(v.Password),[]byte(pwd1))==nil{
+			//set claims
+			claims = UserClaims{
+				User{Id:v.Id,FirstName:v.FirstName,LastName:v.LastName,UserType:v.UserType,OrgId:v.OrgId},
+				time.Now(),                       //to generate unique token everytime  //rand.Intn(10000),
+				jwt.StandardClaims{
+					Issuer: "testing_administrator",
+				},
 			}
+
+			token := jwt.NewWithClaims(jwt.SigningMethodRS256,claims)
+			ss, err := token.SignedString(SignKey)
+
+			if err != nil {
+				c.String(404,"Not Found")
+				fmt.Fprintln(c.Writer,err)
+				log.Printf("err: %+v\n", err)
+				return
+			}
+
+			c.Writer.Header().Set("status","200")
+			response := Token{ss}
+			JsonResponse(response,"200 OK",c.Writer)
+			flag=1
+			return
+		}else {
+			flag=-1
 		}
 	}
-
-	if flag !=1{
-		token := jwt.NewWithClaims(jwt.SigningMethodRS256,claims)
-		ss, err := token.SignedString(SignKey)
-
-		if err != nil {
-			c.String(404,"Not Found")
-			fmt.Fprintln(c.Writer,err)
-			log.Printf("err: %+v\n", err)
-			return
-		}
-
-		// create session
-		cookie,err:=c.Request.Cookie("session")
-
-		if err!=nil{
-			cookie = &http.Cookie{
-				Name:  "session",
-				Value: ss,
-				Expires:time.Now().Add(time.Second*300),
-			}
-		}else{
-			cookie.Value=ss
-		}
-
-		http.SetCookie(c.Writer, cookie)
-
-		c.Writer.Header().Set("status","200")
-		response := Token{ss}
-		JsonResponse(response, c.Writer)
+	if flag==-1{
+		fmt.Println("Error Logging in")
+		c.AbortWithStatusJSON(401,"Error Logging in")
 	}
 }
 
-func authMiddleware(c *gin.Context) (success bool){
+func authMiddleWare() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		//validate token
+		token, err := jwtreq.ParseFromRequestWithClaims(context.Request, jwtreq.AuthorizationHeaderExtractor,&claims,func(token *jwt.Token) (interface{}, error){
+			return VerifyKey, nil
+		})
 
-	//validate token
-	token, err := jwtreq.ParseFromRequestWithClaims(c.Request, jwtreq.AuthorizationHeaderExtractor,&claims,func(token *jwt.Token) (interface{}, error){
-		return VerifyKey, nil
-	})
-
-	if err == nil {
-
-		if token.Valid{
-			fmt.Println("VERIFIED !")
-			fmt.Fprint(c.Writer, "SUCCESS.....!!")
-			return true
+		if err == nil && token.Valid{
+			fmt.Println("VERIFIED Logged In !")
+			context.Next()
 		} else {
-			c.Writer.Header().Set("status","401")
-			fmt.Fprint(c.Writer, "Token is not valid")
-			return false
+			JsonResponse("Unauthorized Access","401",context.Writer)
+			context.Abort()
 		}
-
-	} else {
-		c.Writer.Header().Set("status","401")
-		fmt.Fprint(c.Writer, "Unauthorised access , Error Verifying Token")
-		return false
 	}
 }
 
 func getProducts(c *gin.Context){
-	if authMiddleware(c)!=true{
-		c.Writer.Header().Set("status","401")
-		fmt.Fprint(c.Writer, "Token is not valid")
-		return
-	}
 
-    var org Organization
+	db:=initDb("xshowroom_auth")
+	defer db.Close()
+
+	var org Organization
 	db.Debug().Model(&Organization{}).
 		Select("DISTINCT x_org_ext.name").
 		Joins("INNER JOIN x_user ON x_org_ext.id = ?",claims.UserProfile.OrgId).
 		Scan(&org)
 
-		fmt.Println(org.Name)      //check
-
-	db_prod,err:=gorm.Open("mysql","root:password@/"+org.Name+"?charset=utf8&parseTime=True&loc=Local")
-
-	if err!=nil {
-		log.Fatal(err)
-	}
-
-	err = db_prod.DB().Ping()
-	if err != nil {
-		log.Fatal(err)
-	}else{
-		fmt.Println("connected with DB ::--",org.Name)
-	}
-	defer db_prod.Close()
+	dbProd:=initDb(org.Name)
+	defer dbProd.Close()
 
 	var products []Product
-	db_prod.Model(&Product{}).Find(&products)
+	dbProd.Model(&Product{}).Find(&products)
+	JsonResponse(products,"200",c.Writer)
 
-	JsonResponse(products,c.Writer)
 }
 
 func getUsers(c *gin.Context){
 	fmt.Println("type :: ",claims.UserProfile.UserType)
 
-	if claims.UserProfile.UserType=="user" || authMiddleware(c)==false{
-			c.Writer.Header().Set("status","401")
-			fmt.Fprint(c.Writer, "No Admin Rights to View all Users!")
+	db:=initDb("xshowroom_auth")
+	defer db.Close()
 
-	}else{
+	if claims.UserProfile.UserType=="admin" {
 		var users []User
 		db.Model(&User{}).Find(&users)
-		JsonResponse(users,c.Writer)
-	}
-}
-
-func createUsers(c *gin.Context){
-
-	if claims.UserProfile.UserType=="user" || authMiddleware(c)==false{
-		c.Writer.Header().Set("status","401")
-		fmt.Fprint(c.Writer, "No Admin Rights to Create Users!")
-	}else{
-		fname:=c.PostForm("first_name")
-		lname:=c.PostForm("last_name")
-		email:=c.PostForm("email")
-		pwd:=c.PostForm("password")
-		phone:=c.PostForm("phone_number")
-		add1:=c.PostForm("address_line1")
-		add2:=c.PostForm("address_line2")
-		utype:=c.PostForm("user_type")
-		oid:=c.PostForm("org_id")
-		orgid,_:=strconv.Atoi(oid)
-		new_user:=User{FirstName:fname,LastName:lname,Email:email,Password:pwd,PhoneNumber:phone,AddressLine1:add1,
-		AddressLine2:add2,UserType:utype,OrgId:uint(orgid)}
-		db.Create(&new_user)
+		JsonResponse(users, "200", c.Writer)
+	}else {
+		JsonResponse("Not an Admin", "401", c.Writer)
 	}
 }
 
 func getOrg(c *gin.Context){
 	fmt.Println("type :: ",claims.UserProfile.UserType)
 
-	if claims.UserProfile.UserType=="user" || authMiddleware(c)==false{
-		c.Writer.Header().Set("status","401")
-		fmt.Fprint(c.Writer, "No Admin Rights to View all Users!")
+	db:=initDb("xshowroom_auth")
+	defer db.Close()
 
-	}else{
+	if claims.UserProfile.UserType=="admin" {
 		var org []Organization
 		db.Model(&Organization{}).Find(&org)
-		JsonResponse(org,c.Writer)
+		JsonResponse(org, "200", c.Writer)
+	}else {
+		JsonResponse("Not an Admin", "401", c.Writer)
 	}
+}
+
+func createUsers(c *gin.Context){
+
+	db:=initDb("xshowroom_auth")
+	defer db.Close()
+
+	if claims.UserProfile.UserType=="admin"{
+		var user User
+		json.NewDecoder(c.Request.Body).Decode(&user)
+
+		bs,_:=bcrypt.GenerateFromPassword([]byte(user.Password),bcrypt.MinCost)
+		user.Password=string(bs)
+
+		db.Create(&user)
+		JsonResponse("User Record Created","200",c.Writer)
+	}else{
+		JsonResponse("Not an admin","401",c.Writer)
+	}
+
 }
 
 func createOrg(c *gin.Context){
 
-	if claims.UserProfile.UserType=="user" || authMiddleware(c)==false{
-		c.Writer.Header().Set("status","401")
-		fmt.Fprint(c.Writer, "No Admin Rights to Create Users!")
+	db:=initDb("xshowroom_auth")
+	defer db.Close()
+
+	if claims.UserProfile.UserType=="admin"{
+		var org Organization
+
+		json.NewDecoder(c.Request.Body).Decode(&org)
+
+		db.Create(&org)
+		fmt.Println(org)
+
+		db.Exec("CREATE DATABASE "+org.Name)
+
+		dbOrg:=initDb(org.Name)
+		defer dbOrg.Close()
+
+		dbOrg.CreateTable(&Product{})
+		JsonResponse("Organisation Record Created","200",c.Writer)
 	}else{
-		name:=c.PostForm("name")
-		add1:=c.PostForm("address_line1")
-		add2:=c.PostForm("address_line2")
-		city:=c.PostForm("city")
-		state:=c.PostForm("state")
-		country:=c.PostForm("country")
-		new_org:=Organization{Name:name,AddressLine1:add1,AddressLine2:add2,City:city,State:state,Country:country}
-
-		db.Create(&new_org)
-		fmt.Println(new_org)
-
-		db.Exec("CREATE DATABASE "+new_org.Name)
-
-		db_org,err:=gorm.Open("mysql","root:password@tcp(127.0.0.1:3306)/"+new_org.Name+"?charset=utf8&parseTime=True&loc=Local")
-
-		if err!=nil {
-			log.Fatal(err)
-		}
-
-		err = db_org.DB().Ping()
-		if err != nil {
-			log.Fatal(err)
-		}else{
-			fmt.Println("connected with DB ::--",new_org.Name)
-		}
-		defer db_org.Close()
-		db_org.CreateTable(&Product{})
+		JsonResponse("No Admin Rights","401",c.Writer)
 	}
 }
 
-func JsonResponse(response interface{}, w http.ResponseWriter) {
+func JsonResponse(response interface{}, status string,w http.ResponseWriter) {
 
-	jsondata, err :=  json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsondata)
+	fmt.Println(response)
+	data:=Datastruct{Response:response,StatusCode:status}
+	json.NewEncoder(w).Encode(data)
 
 }
-
